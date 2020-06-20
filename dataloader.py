a69d2eee8761db5c12b06860cbd4a7ca04fe2508
Anil Yadav <ayadav01@student.coppin.edu>
2020-06-19 20:01:22 -0700

    dataloader changes from original code

--- dataloader.py
+++ dataloader.py
@@ -5,6 +5,7 @@ import sys
 import csv
 
 class LungDataset(Dataset):
+
     """CSV dataset"""
     def __init__(self, train_file, num_tasks=3, transform=None):
         """
@@ -14,15 +15,17 @@ class LungDataset(Dataset):
         """
         self.train_file = train_file
         self.transform = transform
-        self.num_tasks = num_tasks
+        self.num_tasks = num_tasks  # 3
         # parse the provided train file
         try:
             with self._open_for_csv(self.train_file) as file:
+                # extracted data
                 self.image_data = self._read_labels(csv.reader(file, delimiter=','))
         except ValueError as e:
-            raise(ValueError('invalid CSV train file: {}: {}'.format(self.train_file, e)), None)
+            raise_from(ValueError('invalid CSV train file: {}: {}'.format(self.train_file, e)), None)
         self.image_names = list(self.image_data.keys())
 
+    """Returns file reader object"""
     def _open_for_csv(self, path):
         """
         Open a file with flags suitable for csv.reader.
@@ -34,6 +37,7 @@ class LungDataset(Dataset):
         else:
             return open(path, 'r', newline='')
 
+    """Returns totoal number of rows in csv"""
     def __len__(self):
         return len(self.image_names)
 
@@ -45,22 +49,35 @@ class LungDataset(Dataset):
             sample = self.transform(sample)
         return sample
 
+    """Reads image and returns numpy array"""
     def load_image(self, image_index):
         img = np.load(self.image_names[image_index])
-        return img.astype(np.float32) / 255.0
+        return img.astype(np.float32)
+        # return img.astype(np.float32)/255.0
 
+    """Loads labels for given images from the dictionary created by '_read_labels()' function"""
     def load_labels(self, image_index):
-        num_labels = self.num_tasks + 1
+        # get num of labels
+        num_labels = self.num_tasks + 1  # 4
+        # print("num_labels:", num_labels)
+        # Gives a list of dictionary with keys as 'label1' and values as value for that label (0)
         label_list = self.image_data[self.image_names[image_index]]
+        # print("label list for {} is {}".format(self.image_names[image_index], label_list))
         labels = np.zeros((0, num_labels))
         # parse labels
         for idx, a in enumerate(label_list):
             label = np.zeros((1, num_labels))
             for i in range(num_labels):
                 label[0, i] = a['label' + str(i + 1)]
+
             labels = np.append(labels, label, axis=0)
         return labels
 
+    """
+    Returns labels dictionaryfor a particular image (CHANGES FROM ORIGINAL CODE)
+    ------------
+    FORMAT: {file_path: [{label0: first_col}, {"label1": second_col}, {"label2": third_col}, {"label3": forth_col}...]}
+    """
     def _read_labels(self, csv_reader):
         result = {}
         num_cols = self.num_tasks + 2
@@ -68,13 +85,16 @@ class LungDataset(Dataset):
             line += 1
             label_dict = {}
             try:
-                img_file = col[-1]
-                for i in range(0, len(col) - 1):
-                    label_dict['label' + str(i + 1)] = col[i]
+                img_file = col[0]
+                # print("label4", img_file) # gets the last label: malignancy score
+                for i in range(1, len(col)):
+                    label_dict['label' + str(i)] = col[i]
+                # print(label_dict)
             except ValueError:
-                raise(ValueError(
+                raise_from(ValueError(
                     'line {}: format should be \'label1,label2,label3,label4, img_file\' or \',,,,img_file\''.format(
                         line)), None)
+            # If label is not in dictionary, create a key with empty list as dict values
             if img_file not in result:
                 result[img_file] = []
             result[img_file].append(label_dict)
