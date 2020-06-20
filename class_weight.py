f81c01b38819739286bfefbd6910751ef4224f0f
Anil Yadav <ayadav01@student.coppin.edu>
2020-06-19 21:08:47 -0700

    orginal class weights code

--- class_weight.py
+++ class_weight.py
@@ -5,7 +5,7 @@ import torch
 def class_weight(train_path, num_tasks=3):
     df = pd.read_csv(train_path, header=None)
     label_list = []
-    for i in range(1, num_tasks + 2):
+    for i in range(num_tasks + 1):
         label_list.append(df.iloc[:, i].tolist())
     class_weight_dict = {}
     for i in range(len(label_list)):
@@ -16,6 +16,5 @@ def class_weight(train_path, num_tasks=3):
             count = float(labels.count(int(j)))
             weight = 1 / (count / float(len(labels)))
             weight_list.append(weight)
-
         class_weight_dict[i] = torch.FloatTensor(weight_list).cuda()
     return class_weight_dict
